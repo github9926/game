6fb8b80a6683e2bb1ec1600204979879cd52d262
Esa-Matti Miettinen <>
2021-01-23 19:58:10 +0200

    Rest api returns player count

--- src/main/java/fi/esamatti/game/AccountResource.java
+++ src/main/java/fi/esamatti/game/AccountResource.java
@@ -3,14 +3,20 @@ package fi.esamatti.game;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Path("/accounts")
 @Service
 public class AccountResource {
+	@Autowired
+	private PlayerRepository playerRepository;
+	
     @GET
-    public String getEmployee() {
-    	System.out.println("Printing");
-        return "someResult";
+    public String getPlayerCount() {
+    	final long playerCount = playerRepository.count();
+    	System.out.println("Players: " + playerCount);
+    	
+		return "Player count: " + playerCount;
     }
 }
