17c910e6ea64c5c862d260dc5fd412059f61bbe6
Esa-Matti Miettinen <>
2021-01-23 20:28:45 +0200

    Use transactions.

--- src/main/java/fi/esamatti/game/AccountResource.java
+++ src/main/java/fi/esamatti/game/AccountResource.java
@@ -5,6 +5,8 @@ import javax.ws.rs.Path;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Path("/accounts")
 @Service
@@ -13,6 +15,7 @@ public class AccountResource {
 	private PlayerRepository playerRepository;
 	
     @GET
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
     public String getPlayerCount() {
     	final long playerCount = playerRepository.count();
     	System.out.println("Players: " + playerCount);
