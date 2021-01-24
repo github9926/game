43fdf395377a1e80c2f1fbc383c1644a3da92d51
Esa-Matti Miettinen <>
2021-01-24 13:57:23 +0200

    SSL connections are required

--- src/test/java/fi/esamatti/game/AccountResourceTest.java
+++ src/test/java/fi/esamatti/game/AccountResourceTest.java
@@ -1,25 +1,57 @@
 package fi.esamatti.game;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.fail;
 
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.io.Resource;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.web.client.RestTemplate;
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations="classpath:test-application.properties")
 class AccountResourceTest {
 	@LocalServerPort
 	private int port;
 	@Autowired
 	private TestRestTemplate restTemplate;
 	
+    @Value("${test.truststore.location}")
+    private Resource trustStore;
+    
+    @Value("${test.truststore.password}")
+    private String trustStorePassword;
+	
 	@Test
 	public void firstRestTest() throws Exception {
-		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/accounts",
+		assertThat(this.restTemplate.getForObject("https://localhost:" + port + "/accounts",
 				String.class)).contains("Player count: 3");
+	}
+	
+	@Test
+	public void httpsRequired() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/accounts",
+				String.class)).contains("Bad Request");
+	}
+	
+	RestTemplate restTemplate() throws Exception {
+		SSLContext sslContext = new SSLContextBuilder()
+				.loadTrustMaterial(trustStore.getURL(), trustStorePassword.toCharArray()).build();
+		SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext);
+		HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory).build();
+		HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+		return new RestTemplate(factory);
 	}
 
 }
