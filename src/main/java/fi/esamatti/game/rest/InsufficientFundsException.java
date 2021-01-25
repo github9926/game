6473ba4917e4f1e89e329dea24cc798388601338
Esa-Matti Miettinen <>
2021-01-25 18:18:14 +0200

    Removed unneeded code

--- src/main/java/fi/esamatti/game/rest/InsufficientFundsException.java
+++ src/main/java/fi/esamatti/game/rest/InsufficientFundsException.java
@@ -1,25 +1,10 @@
 package fi.esamatti.game.rest;
 
-import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.ExceptionMapper;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.server.ResponseStatusException;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Actor Not Found")
-public class InsufficientFundsException extends ResponseStatusException
-		implements ExceptionMapper<InsufficientFundsException> {
+public class InsufficientFundsException extends RuntimeException {
 
 	private static final long serialVersionUID = -7615222151693860303L;
 
 	public InsufficientFundsException(final Long balance, final Long buyAmount) {
-		super(HttpStatus.CONFLICT,
-				"Insufficient funds! Account only has " + balance + ", but tried to buy " + buyAmount);
-	}
-
-	@Override
-	public Response toResponse(final InsufficientFundsException exception) {
-		return Response.status(404).entity(exception.getMessage()).type("text/plain").build();
+		super("Insufficient funds! Account only has " + balance + ", but tried to buy " + buyAmount);
 	}
 }
